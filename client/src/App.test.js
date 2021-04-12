d4892a16f8ead0f02b8e4ec57ec91394f952b41a
lee-injae <ilee.injae@gmail.com>
2021-04-12 13:52:20 -0400

    initial commit

---
+++ client/src/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+
+it('renders without crashing', ()=> {}); 
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+// });
